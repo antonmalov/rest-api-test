f105e2139138739e92f7e912a7121e104638e55b
Anton Malov <maloanto@yandex.ru>
2023-06-25 18:25:10 +0300

    added first tests for api

--- src/test/java/SelenoidTests.java
+++ src/test/java/SelenoidTests.java
@@ -1,2 +1,21 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.is;
+
 public class SelenoidTests {
+
+    @Test
+    void checkTotal() {
+        get("http://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalAndStatus() {
+        get("http://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
 }
